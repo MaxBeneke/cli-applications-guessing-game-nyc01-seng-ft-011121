ef4d6e89acaaa1f1ae028018af116665c12da467
Max Beneke <max.beneke@gmail.com>
2020-12-30 15:06:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,27 @@
-# Code your solution here!
+def generate_number
+  rand(6) + 1 
+end
+
+def ask_for_input
+  puts "Guess a number, 1-6!"
+end
+
+def get_input
+  gets.chomp
+end
+
+def run_guessing_game
+  answer = generate_number
+  ask_for_input
+  input = get_input
+  
+  if answer == input.to_i
+   puts "You guessed the correct number!"
+   
+  elsif answer != input.to_i
+   puts "Sorry! The computer guessed #{answer}" 
+   
+ elsif input == "exit"
+  puts "Goodbye!"
+end
+end
